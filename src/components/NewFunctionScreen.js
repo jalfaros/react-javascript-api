315dfb06a17a0f88781becd9552d22faa78b0d03
Jose Ignacio Alfaro <ignacioalfaro99@gmail.com>
2022-01-17 22:33:43 -0600

    Agreagr nueva categoría

--- src/components/NewFunctionScreen.js
+++ src/components/NewFunctionScreen.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap'
+import Swal from 'sweetalert2';
 import { NewCategory } from '../components/ModalCategory/NewCategory'
 import { useForm } from '../hooks/useForm';
 import '../styles/function.css';
 import NewFunctionForm from './NewFunctionForm';
+
+
+const BASEURL = 'http://localhost:8080/api/functions'
 
 const NewFunctionScreen = () => {
 
@@ -16,19 +20,52 @@ const NewFunctionScreen = () => {
         idCategoria: "0"
     });
 
-
+    
     const handleSubmit = ( e ) => {
         e.preventDefault();
         console.log( formValues )
         //Fetch al guardar función
     };
 
-    const handleModalSubmit = ( e ) => {
+    const handleModalSubmit = async ( e ) => {
+
         e.preventDefault();
 
-        //Fetch al nueva categoría
-        console.log( e.target[0].value )
+        try{
+            let response = await fetch( BASEURL + '/newCategory',  {
+                method: 'POST',
+                body: JSON.stringify({nombreCategoria: e.target[0].value }),
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'x-token' : JSON.parse( localStorage.getItem('token') ) || ''   
+                }
+            });
+
+            handleFetchModal( await response.json() );
+
+        }catch( err ){
+            console.log( err )
+        }
     };
+
+    const handleFetchModal = ( response ) => {
+
+        if ( response.code !== 200 ){
+
+            const { content } = response;
+
+            Swal.fire({
+                icon: 'error',
+                text: content.errors[1],
+                timer: 1500,
+                showConfirmButton: false
+            });
+            return;
+        };
+        // Categorías propias del usuario, se deben de devolver todas las del id correspondiente
+        setShow( !show )
+    };
+
 
     const handleShowModal = () => {
         setShow( !show )
