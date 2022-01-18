09af40a07e93ab0e84802584e6199e35f2d03fdd
Jose Ignacio Alfaro <ignacioalfaro99@gmail.com>
2022-01-17 21:23:41 -0600

    Cambios en la estructura del NewFuntionScreen

--- src/components/NewFunctionScreen.js
+++ src/components/NewFunctionScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap'
+import { NewCategory } from '../components/ModalCategory/NewCategory'
 import { useForm } from '../hooks/useForm';
 import '../styles/function.css';
+import NewFunctionForm from './NewFunctionForm';
 
 const NewFunctionScreen = () => {
 
+    const [ show, setShow ] = useState(false);
+        
     const [ formValues, handleInputChange ] = useForm({
         nombreFuncion: '',
         descripcion: '',
@@ -12,75 +16,48 @@ const NewFunctionScreen = () => {
         idCategoria: "0"
     });
 
-    const { nombreFuncion, descripcion, codigoFuncion, idCategoria } = formValues;
 
     const handleSubmit = ( e ) => {
         e.preventDefault();
-
         console.log( formValues )
+        //Fetch al guardar función
     };
 
-    // useEffect(() => {
-    //     console.log('ejecución')
-    //     try {
-    //         eval( codigoFuncion )
-    //     }catch( err ){
-    //         console.log( err )
-    //     }
-    // }, [codigoFuncion])
+    const handleModalSubmit = ( e ) => {
+        e.preventDefault();
+
+        //Fetch al nueva categoría
+        console.log( e.target[0].value )
+    };
+
+    const handleShowModal = () => {
+        setShow( !show )
+    };
 
 
     return (
+        
         <Card>
             <Card.Title className='text-center' style={{ marginTop: '10px' }}>
                 Nueva función
             </Card.Title>
             <hr />
             <Card.Body>
+
+            <NewCategory  
+                show = { show }
+                setShow = { setShow }
+                handleModalSubmit={ handleModalSubmit }
                 
-            <form className = "formFlex" onSubmit={ handleSubmit } autoComplete='off'>
+                
+            />
 
-                <label htmlFor="nombreFuncion">Nombre</label>
-                <input  type="text" 
-                        name = "nombreFuncion"
-                        id = "nombreFuncion"
-                        placeholder='Fibonacci...'
-                        onChange={ handleInputChange }
-                        value = { nombreFuncion }
-                />
-
-                <label htmlFor="descripcion">Descripción<span>*</span></label>
-                <input  type="text" 
-                        name = "descripcion"
-                        id = "descipcion"
-                        placeholder='Recursiva...'
-                        required
-                        onChange={ handleInputChange }
-                        value = { descripcion }
-                />
-
-                <label htmlFor="idCategoria">Categoría<span>*</span></label>
-
-                <select name="idCategoria" id="idCategoria" required onChange={handleInputChange} value = { idCategoria }>
-
-                </select>
-
-                <label htmlFor="codigoFuncion">Código<span>*</span></label>
-                <textarea   name="codigoFuncion" 
-                            id="codigoFuncion" 
-                            cols="5" 
-                            rows="5" 
-                            required
-                            onChange={ handleInputChange }
-                            value = { codigoFuncion }
-                />
-
-
-                <Button variant="outline-primary" type="submit"> 
-                    <i className="far fa-save"></i>
-                </Button>
-
-            </form>
+            <NewFunctionForm 
+                handleShowModal = { handleShowModal }
+                formValues = { formValues }
+                handleSubmit = { handleSubmit }
+                handleInputChange={ handleInputChange }
+            />
 
             </Card.Body>
         </Card>
